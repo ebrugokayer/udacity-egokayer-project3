9079931861f7c1b09eb213ce7f03d4a0cabb3e05
ebrugokayer <ebru.gokay@turkcell.com.tr>
2020-09-09 00:14:29 +0300

    CORS !!!

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -26,6 +26,8 @@ import { V0_FEED_MODELS } from "./controllers/v0/model.index";
         "Accept",
         "X-Access-Token",
         "Authorization",
+        "Access-Control-Allow-Origin",
+        "*",
       ],
       methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
       // origin: config.url,
